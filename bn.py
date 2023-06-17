0df326230d00524dc1104362fd6d9ef48bd46204
Mehslos <134284173+Mehslos@users.noreply.github.com>
2023-06-17 23:20:56 +0530

    Update bn.py

--- bn.py
+++ bn.py
@@ -1,9 +1,4 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -20,25 +15,18 @@ import string
 import os
 from selenium.webdriver.common.by import By
 
-
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-
-chrome_options = Options()
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1366, 784",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-for option in options:
-    chrome_options.add_argument(option)
-
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+rand_name = names.get_first_name(gender='male')
+S = random.randint(4,8)
+rn = ''.join(random.choices(string.ascii_lowercase + string.digits, k = S))    
+names=(rand_name+rn)
+ran = ''.join(random.choices(string.ascii_uppercase+string.ascii_lowercase + string.digits, k = S))    
+main= (rand_name+ran+'@gmail.com')
+main2 = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(random.randint(8,12))))
 
 
+
+driver = webdriver.Chrome()
+#driver.fullscreen_window()
 driver.get('https://dgsmtt.aduacademy.in/')
 time.sleep(10)
 
@@ -60,7 +48,16 @@ driver.find_element(by=By.XPATH, value='//*[@id="frmstudenthome"]/fieldset/div[1
 time.sleep(10)
 
 
-driver.find_element(by=By.XPATH, value='//*[@id="ninext"]').click()
-time.sleep(100)
+driver.find_element(by=By.XPATH, value='//*[@id="tretoc"]/div/ul/ul/ul[2]/ul[4]/li[3]/span').click()
+time.sleep(1200)
+
+driver.find_element(by=By.XPATH, value='//*[@id="tretoc"]/div/ul/ul/ul[2]/ul[4]/li[4]/span').click()
+time.sleep(1600)
 
 
+driver.find_element(by=By.XPATH, value='//*[@id="tretoc"]/div/ul/ul/ul[2]/ul[4]/li[5]/span').click()
+time.sleep(1400)
+
+
+driver.find_element(by=By.XPATH, value='//*[@id="tretoc"]/div/ul/ul/ul[1]/ul[1]/li[1]/span').click()
+time.sleep(1500)
