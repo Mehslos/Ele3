c38be609f7d7f9b879adfa1f8bf13437f472dbda
Mehslos <134284173+Mehslos@users.noreply.github.com>
2023-06-17 22:27:50 +0530

    Update bn.py

--- bn.py
+++ bn.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -15,18 +20,25 @@ import string
 import os
 from selenium.webdriver.common.by import By
 
-rand_name = names.get_first_name(gender='male')
-S = random.randint(4,8)
-rn = ''.join(random.choices(string.ascii_lowercase + string.digits, k = S))    
-names=(rand_name+rn)
-ran = ''.join(random.choices(string.ascii_uppercase+string.ascii_lowercase + string.digits, k = S))    
-main= (rand_name+ran+'@gmail.com')
-main2 = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(random.randint(8,12))))
+
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 
-
-driver = webdriver.Chrome()
-driver.fullscreen_window()
 driver.get('https://dgsmtt.aduacademy.in/')
 time.sleep(10)
 
@@ -48,13 +60,7 @@ driver.find_element(by=By.XPATH, value='//*[@id="frmstudenthome"]/fieldset/div[1
 time.sleep(10)
 
 
-#driver.find_element(by=By.XPATH, value='//*[@id="ebe87014"]/span').click()
-#time.sleep(10)
-
-
-while True:
-	
-	driver.find_element(by=By.XPATH, value='//*[@id="ninext"]').click()
-	time.sleep(100)
+driver.find_element(by=By.XPATH, value='//*[@id="ninext"]').click()
+time.sleep(100)
 
 
